891e5469e4bba6436e7de91535963f1c2e4543cf
martinho <martinho.n03@gmail.com>
2024-02-20 11:31:05 +0200

    fix: remove warnings of deprecation
    properties in the .connect() useNewUrlParser and useUnifiedTopology

--- IoT-Final/server.js
+++ IoT-Final/server.js
@@ -6,42 +6,40 @@ import { MongoClient } from 'mongodb';
 const app = express();
 
 const PORT=6969
-const mongoURI="mongodb://root:example@localhost:27017"
-
+// TODO: try using a time-series database like InfluxDB
+const MONGO_URI="mongodb://root:example@localhost:27017"
 const dbName = 'Weather';
 
 // MQTT Broker configuration
-const mqttBroker = 'mqtt://172.20.49.49';
-const mqttClient = mqtt.connect(mqttBroker);
+const MQTT_BROKER = 'mqtt://172.20.49.49';
+const mqttClient = mqtt.connect(MQTT_BROKER);
 
 // Connect to MongoDB
-MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(client => {
-        console.log('Connected to MongoDB');
-        const db = client.db(dbName);
+MongoClient.connect(MONGO_URI).then(client => {
+    console.log('Connected to MongoDB');
+    const db = client.db(dbName);
 
-        // MQTT subscription
-        mqttClient.on('connect', () => {
-            console.log('Connected to MQTT broker');
-            mqttClient.subscribe('weatherData');
-        });
-
-        mqttClient.on('message', (topic, message) => {
-            console.log('Received message from MQTT:', message.toString());
-            const data = JSON.parse(message);
-            // Store data in MongoDB
-            db.collection('data').insertOne(data, (err, result) => {
-                if (err) {
-                    console.error('Failed to insert data into MongoDB:', err);
-                    return;
-                }
-                console.log('Data inserted into MongoDB:', result.ops);
-            });
-        });
-    })
-    .catch(err => {
-        console.error('Failed to connect to MongoDB:', err);
+    // MQTT subscription
+    mqttClient.on('connect', () => {
+        console.log('Connected to MQTT broker');
+        mqttClient.subscribe('weatherData');
     });
+
+    mqttClient.on('message', (topic, message) => {
+        console.log('Received message from topic:', topic.toString(), message.toString());
+        const data = JSON.parse(message);
+        // Store data in MongoDB
+        db.collection('data').insertOne(data, (err, result) => {
+            if (err) {
+                console.error('Failed to insert data into MongoDB:', err);
+                return;
+            }
+            console.log('Data inserted into MongoDB:', result.ops);
+        });
+    });
+}).catch(err => {
+    console.error('Failed to connect to MongoDB:', err);
+});
 
 // Start the Express server
 app.listen(PORT, () => {

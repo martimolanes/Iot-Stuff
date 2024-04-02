c741c7903f798453da29a5d583c58caef1512ed3
martinho <martinho.n03@gmail.com>
2024-04-02 11:51:25 +0300

    repo: env variables used with the start script

--- start-service.sh
+++ start-service.sh
@@ -10,33 +10,51 @@ function help() {
     echo "  run:"
     echo "    $0 mqtt-broker      - start mqtt service"
     echo "    $0 db               - start database service with visualization (influxdb, grafana)"
-    echo "    $0 server           - start server"
-    echo "    $0 api              - start api"
+    echo "    $0 server           - start server (write data to influxdb)"
+    echo "    $0 api              - start api (expose data from influxdb)"
     echo "  test scripts:"
-    echo "    $0 publish-mqtt     - mock esp32 arduino"
+    echo "    $0 publish-mqtt     - mock esp32 arduino (publish data to mqtt broker)"
     echo "  stop:"
-    echo "    $0 stop             - stop all docker-services"
+    echo "    $0 stop             - stop all project services"
 }
 
 function start_mqtt() {
+    source ./mqtt-broker/.env 2>/dev/null
     echo "Starting mqtt service"
     docker-compose -f ./mqtt-broker/mqtt-broker.yml up -d
+    echo "----------------------------------------"
+    echo "Accessing the Mosquitto:"
+    echo "----------------------------------------"
+    echo "  - Access the mosquitto at http://localhost:${MQTT_PORT:-1883}"
 }
 
 function start_database() {
+    source ./influx-grafana/.env 2>/dev/null
     echo "Starting influxdb and grafana services"
     docker-compose -f ./influx-grafana/influx-grafana.yml up -d
+    echo "----------------------------------------"
+    echo "Accessing the services:"
+    echo "----------------------------------------"
+    echo "InfluxDB:"
+    echo "  - Access the InfluxDB at http://localhost:${INFLUXDB_PORT:-8086}"
+    echo "  - Username: ${INFLUXDB_USER:-my-user}"
+    echo "  - Password: ${INFLUXDB_PASSWORD:-my-password}"
+    echo "----------------------------------------"
+    echo "Grafana:"
+    echo "  - Access Grafana at http://localhost:${GRAFANA_PORT:-3000}"
+    echo "----------------------------------------"
 }
 
 function start_server() {
     echo "Starting server"
+    echo "Data is readed from mqtt-broker and written to influxdb"
     cd server
     npm ci
     npm start
 }
 
 function start_api() {
-    echo "Starting api"
+    echo "Starting weather api"
     docker build -t go-api-weather-station api/
     docker run --rm --name go-api --network="host" -d go-api-weather-station
 }
@@ -46,7 +64,7 @@ function publish_mqtt() {
 }
 
 function stop_services() {
-    echo "Stopping all services"
+    echo "Stopping all weather station services"
     docker-compose -f ./mqtt-broker/mqtt-broker.yml down
     docker-compose -f ./influx-grafana/influx-grafana.yml down
 }
